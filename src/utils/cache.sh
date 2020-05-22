15fc771805e1ba06ae3060a2116c00d964d6eb31
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-05-22 12:46:54 +0200

    Cache: own file, finer controls

---
+++ src/utils/cache.sh
@@ -0,0 +1,75 @@
+# shellcheck shell=bash
+# shellcheck source=../vars.sh
+. ""
+
+#
+# Caches the return code and echoed string of a function
+# DO NOT call from subshell: memory would not be written, script will die
+#
+cache() {
+  local args=("$@")
+  local cache_key="$1"
+  local read_cache="$2"
+  local write_cache="$3"
+  local cmd="$4"
+
+  local string
+  local code
+
+  # Read from cache or execute
+  if $read_cache && cache.has "$cache_key"; then
+    string=$(cache.get_string "$cache_key")
+    code=$(cache.get_code "$cache_key")
+  else
+    args=("${args[@]:4}")
+
+    string=$($cmd "${args[@]}")
+    code="$?"
+
+    # Write to cache
+    if $write_cache; then
+      # NOTE cannot write cache in a subshell
+      if helpers.is_subshell; then
+        # Terminating whole script
+        echo "HELPERS.CACHE CANNOT WRITE IN A SUBSHELL (key: $cache_key, cmd: $cmd)"
+        kill $$
+        exit
+      fi
+
+      cache.set "$cache_key" "$string" "$code"
+    fi
+  fi
+
+  # Echo string and return code
+  string.is_empty "$string" || echo "$string"
+  return $code
+}
+
+#
+# Returns true if cache has data for in key $1, false otherwise
+#
+cache.has() {
+  helpers.is_set "${__cache[$1]}"
+}
+
+#
+# Gets cached string for key $1
+#
+cache.get_string() {
+  echo "${__cache[__${1}__string]}"
+}
+
+#
+# Gets cached code for key $1
+#
+cache.get_code() {
+  echo "${__cache[__${1}__code]}"
+}
+
+#
+# Sets cache in $1 string $2 and code $3
+cache.set() {
+  __cache[$1]=true
+  __cache[__${1}__string]="$2"
+  __cache[__${1}__code]="$3"
+}
--- src/utils/helpers.sh
+++ src/utils/helpers.sh
@@ -36,54 +36,6 @@ helpers.command_exists() {
 helpers.is_subshell() {
   [ "$$" -ne "$BASHPID" ]
 }
-
-#
-# Caches the return code and echoed string of a function
-# DO NOT call from subshell: memory would not be written, script will die
-#
-helpers.cache() {
-  local args=("$@")
-  local cache_key="$1"
-  local read_cache="$2"
-  local write_cache="$3"
-  local cmd="$4"
-
-  local cache_string_key="__${cache_key}__string"
-  local cache_code_key="__${cache_key}__code"
-  local string
-  local code
-
-  # Read from cache or execute
-  if $read_cache && helpers.is_set "${__cache[$cache_key]}"; then
-    string="${__cache[$cache_string_key]}"
-    code=${__cache[$cache_code_key]}
-  else
-    args=("${args[@]:4}")
-
-    string=$($cmd "${args[@]}")
-    code="$?"
-
-    # Write to cache
-    if $write_cache; then
-      # NOTE cannot write cache in a subshell
-      if helpers.is_subshell; then
-        # Terminating whole script
-        echo "HELPERS.CACHE CANNOT WRITE IN A SUBSHELL (key: $cache_key, cmd: $cmd)"
-        kill $$
-        exit
-      fi
-
-      __cache[$cache_key]="true"
-      __cache[$cache_string_key]="$string"
-      __cache[$cache_code_key]=$code
-    fi
-  fi
-
-  # Echo string and return code
-  ! string.is_empty "$string" && echo "$string"
-  return $code
-}
-
 #
 # Removes blanks and duplicates from CSV (stdin)
 #
